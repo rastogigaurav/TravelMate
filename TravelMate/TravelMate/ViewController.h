bad2a284639bcfe8b38b1e24f7ba0f489b3ffba9
Gaurav Rastogi <grastogi@bbmtek.com>
2017-08-05 16:12:27 +0530

    Added Initial Project to the repository

---
+++ TravelMate/TravelMate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TravelMate
+//
+//  Created by Gaurav Rastogi on 8/5/17.
+//  Copyright © 2017 Gaurav Rastogi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TravelMate/TravelMate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TravelMate
+//
+//  Created by Gaurav Rastogi on 8/5/17.
+//  Copyright © 2017 Gaurav Rastogi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
